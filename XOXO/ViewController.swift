b9be44d43f80299804d53e31bad6a8c90df92b71
Buse Karabıyık <karabiyikbuse04@gmail.com>
2024-01-21 17:56:10 +0300

    Initial Commit

---
+++ XOXO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XOXO
+//
+//  Created by Buse Karabıyık on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
